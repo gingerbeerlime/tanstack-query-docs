8e33582c689cdc2b973349f78706978a42f34154
gingerbeerlime <lsumin1127@gmail.com>
2025-08-04 17:45:28 +0900

    chore:: tanstack query dev tool 설치

--- practice/src/main.tsx
+++ practice/src/main.tsx
@@ -1,7 +1,8 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import App from "./App";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -10,12 +11,13 @@ const queryClient = new QueryClient({
       staleTime: 5 * 60 * 1000, // 5분
     },
   },
-})
+});
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
